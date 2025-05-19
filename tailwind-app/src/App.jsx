e7a5cdb0809965ee6d4d1bb3fad6be1347c33194
PeterDayne <chavez.ruiz.pedro607@gmail.com>
2025-05-19 15:37:01 -0600

    propiedades de los componentes

--- tailwind-app/src/App.jsx
+++ tailwind-app/src/App.jsx
@@ -4,21 +4,31 @@ import About from './About';
 import ContactUs from './ContactUs';
 import TeamSection from './TeamSection';
 import NotFound from './NotFound';
+import { useState, useEffect } from "react";
+import Card from './Card';
+// ...
+import ProductGrid from './ProductGrid';
+// ...
+        
+// ...
 
 function App() {
-  
   return (
     <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<NotFound/>}
-        />
+      
+      <Route path="/" element={<ProductGrid/>}/>
       <Route path="Contact" element={<ContactUs/>} />
       <Route path="Team" element={<TeamSection/>} />
       <Route path="About" element={<About/>} />
       <Route path="*" element={<NotFound/>} />
+      
+      
       </Routes>
     </BrowserRouter>
+
+    
 
     </>
   )
